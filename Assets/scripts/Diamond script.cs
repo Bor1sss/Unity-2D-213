7676f964779e9c9806c2700ba3af774ab3edac08
Bor1sss <78493730+Bor1sss@users.noreply.github.com>
2024-10-31 11:32:25 +0200

    Initial commit

---
+++ Assets/scripts/Diamond script.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+public class Diamondscript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
