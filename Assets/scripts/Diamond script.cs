7b19ccf90bfac9a41f7e4796985fcde9c848810d
Bor1sss <78493730+Bor1sss@users.noreply.github.com>
2024-11-04 20:41:29 +0200

    dz 2
    dz2

--- Assets/scripts/Diamond script.cs
+++ Assets/scripts/Diamond script.cs
@@ -1,17 +1,56 @@
 using System;
+using UnityEditor;
 using UnityEngine;
 
 public class Diamondscript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private Rigidbody2D rb2d;
+    private Vector2 startDragPosition;
+    private Vector2 releasePosition;
+    private bool isDragging = false;
+
     void Start()
     {
-        Debug.Log("Hello world");
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.bodyType = RigidbodyType2D.Kinematic;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Начало перетаскивания при нажатии левой кнопкой мыши
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            startDragPosition = Input.mousePosition;
+            isDragging = true;
+            
+        }
+
+        // Завершение перетаскивания и запуск объекта
+        if (Input.GetKeyUp(KeyCode.Mouse0) && isDragging)
+        {
+            releasePosition = Input.mousePosition;
+            rb2d.bodyType = RigidbodyType2D.Dynamic;
+            Vector2 force = (startDragPosition - releasePosition) * 2; 
+            rb2d.AddForce(force);
+            isDragging = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+           rb2d.AddTorque(-100);
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            rb2d.angularVelocity = 0;
+            rb2d.linearVelocity = Vector2.zero;
+        }
         
+        if (isDragging)
+        {
+            Vector2 currentMousePosition = (Input.mousePosition);
+      
+        }
     }
 }
